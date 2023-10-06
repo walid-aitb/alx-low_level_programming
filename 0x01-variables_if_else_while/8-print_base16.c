af6c862470080d74edb9bc99195b63f7e083e73d
walid-aitb <walidaitboussmane@gmail.com>
2023-10-06 13:26:23 -0700

    Updated 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,11 +12,9 @@ int main(void)
 	char letter;
 
 	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
-
+	putchar((num % 10) + '0');
 	for (letter = 'a'; letter <= 'f'; letter++)
-		putchar(letter);
-
-		putchar('\n');
+	putchar(letter);
+	putchar('\n');
 	return (0);
 }
