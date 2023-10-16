293c6622fe70a9589f49332ba872b5c306fe08b6
walid-aitb <walidaitboussmane@gmail.com>
2023-10-16 12:35:01 -0700

    Added 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,15 +6,16 @@
  */
 void print_rev(char *s)
 {
-	int length;
+	int i = 0;
 
-	for (length = 0; s[length] != '\0'; length++)
+	while (s[i])
+		i++;
+
+	while (i--)
 	{
-	
-	for (length = length - 1; length >= 0; length--)
-	{
-		_putchar(s[length]);
+		_putchar(s[i]);
 	}
-	}
+
 	_putchar('\n');
+
 }
